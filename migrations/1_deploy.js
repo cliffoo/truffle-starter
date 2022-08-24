2eb6758d746c812a4445415d8f5a52b207465ef5
Cliff Zhang <cliffzhang08@gmail.com>
2022-08-25 01:01:00 +0800

    Add HelloWorld contract

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,5 +1,7 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
+const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(HelloWorld);
 };
